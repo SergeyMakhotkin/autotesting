be1980d882881483a80ae8cb420a32d5ee751ba9
SergeyMakhotkin <ser9box@mail.ru>
2020-03-12 18:04:57 +0300

    2 способа перехода между Page Object

---
+++ pages/login_page.py
@@ -0,0 +1,26 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+from selenium.common.exceptions import NoSuchElementException
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        print("url check passed!")
+        self.should_be_login_form()
+        print("login form check passed!")
+        self.should_be_register_form()
+        print("register form check passed!")
+        print("It's really loggin page!")
+
+    def should_be_login_url(self):
+        assert 'login' in self.browser.current_url, 'Wrong login URL'
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), \
+            'Login form is not present'
+
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), \
+            'Register form is not present'
